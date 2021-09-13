0b1a2ca22ab17eb986e9958e5ec70de82ea5b40d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-13 21:33:33 +0700

    Set up the project

---
+++ swift-dating-firebase/swift_dating_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_dating_firebaseApp.swift
+//  swift-dating-firebase
+//
+//  Created by Danh Tu on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_dating_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
