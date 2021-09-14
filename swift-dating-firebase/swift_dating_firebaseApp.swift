f8b4db4e635918c9c2446567a63010f31842b26a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-14 10:23:18 +0700

    Connecting Firebase

--- swift-dating-firebase/swift_dating_firebaseApp.swift
+++ swift-dating-firebase/swift_dating_firebaseApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct swift_dating_firebaseApp: App {
+    @UIApplicationDelegateAdaptor(Delegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+// Connecting Firebase
+class Delegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
     }
 }
