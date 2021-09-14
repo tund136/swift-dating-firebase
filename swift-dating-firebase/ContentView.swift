6509f0fce7bb7787d71b95de50b28d0d4c4c29ac
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-14 10:48:23 +0700

    Create loading view

--- swift-dating-firebase/ContentView.swift
+++ swift-dating-firebase/ContentView.swift
@@ -323,6 +323,31 @@ class ModelData: ObservableObject {
     }
 }
 
+// Loading View
+struct LoadingView: View {
+    @State var animation = false
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .trim(from: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, to: 0.7)
+                .stroke(Color.red, lineWidth: 8)
+                .frame(width: 75, height: 75)
+                .rotationEffect(.init(degrees: animation ? 360 : 0))
+                .padding(50)
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.opacity(0.4).ignoresSafeArea())
+        .onAppear(perform: {
+            withAnimation(Animation.linear(duration: 1)) {
+                animation.toggle()
+            }
+        })
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
